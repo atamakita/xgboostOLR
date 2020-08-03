37e39da7474ad98b8b9a0dfd8a4874918e4580e6
shunsuke akita <shunsukeakita@Shunsuke-PC-3.local>
2020-08-03 22:30:26 +0900

    add data generator

---
+++ R/data-scatter.R
@@ -0,0 +1,14 @@
+#' Scatter data
+#'
+#' This is data.
+#'
+#' @format A data.frame including 100 observations and 2 variables
+#' \describe{
+#' \item{type}{character}
+#' \item{cls}{integer represented ordered class}
+#' \item{V1,V2}{numeric}
+#' }
+#'
+#' @example
+#' scatter
+NULL
